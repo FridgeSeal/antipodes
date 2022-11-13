06903b72f08279587d156c96c43ae8c023e419c4
Tom Watson <tom_w@fastmail.com>
2022-11-13 12:38:40 +1100

    Build run_services to start rstudio and jupyter

--- dev/run_services.sh
+++ dev/run_services.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-# TODO
+rstudio-server start
+# Allow root call necessary on jupyter, as root access was a project requirement
+# and jupyter cuts you off from the environment by default
+jupyter lab -ip=0.0.0.0 --allow-root
