9f5d94ac8f3fbc988cbccff8e6c37d98286beac5
Tom Watson <tom_w@fastmail.com>
2022-11-13 16:51:12 +1100

    Set flag on ip correctly

--- dev/run_services.sh
+++ dev/run_services.sh
@@ -4,4 +4,4 @@ set -eux
 rstudio-server start
 # Allow root call necessary on jupyter, as root access was a project requirement
 # and jupyter cuts you off from the environment by default
-jupyter lab -ip=0.0.0.0 --allow-root
+jupyter lab --ip=0.0.0.0 --allow-root
