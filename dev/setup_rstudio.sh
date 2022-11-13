411589fe4ed8210f8e43f5beeaa950912990ab5c
Tom Watson <tom_w@fastmail.com>
2022-11-13 12:37:36 +1100

    Finish setup of rstudio-server

--- dev/setup_rstudio.sh
+++ dev/setup_rstudio.sh
@@ -2,18 +2,19 @@
 set -eux
 
 # Setup R, and dependencies, instructions via R website
-sudo apt update -qq
-sudo apt install --no-install-recommends software-properties-common dirmngr
-wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt-get update -yqq
+apt-get install --no-install-recommends software-properties-common dirmngr wget -yq
+wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
-sudo apt install --no-install-recommends r-base
+apt-get install --no-install-recommends r-base -yq
 
 # Setup Rstudio server reqs
 # instructions via: https://posit.co/download/rstudio-server/
-sudo apt-get install gdebi-core
+apt-get install gdebi-core -yq
 wget https://download2.rstudio.org/server/jammy/amd64/rstudio-server-2022.07.2-576-amd64.deb
-sudo gdebi rstudio-server-2022.07.2-576-amd64.deb
+gdebi -nq rstudio-server-2022.07.2-576-amd64.deb
 
 # Healthcheck install:
-sudo rstudio-server verify-installation
+# rstudio-server verify-installation
+# rstudio starts up on install.
