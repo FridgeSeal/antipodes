2376050310f262bc82fed43371e7ef0b2a565a7a
Tom Watson <tom_w@fastmail.com>
2022-11-16 14:40:14 +1100

    Update SQL script to create etf_data table necessary for first task

--- sql_setup.sql
+++ sql_setup.sql
@@ -1,5 +1,24 @@
 CREATE DATABASE antipodes;
 BEGIN TRANSACTION
+    CREATE TABLE antipodes.dbo.etf_data (
+        ticker NVARCHAR(64) PRIMARY KEY NOT NULL,
+        last_updated  DATE NOT NULL,
+        current_iiv DECIMAL(19,4),
+        change_absolute DECIMAL(19,4),
+        change_percentage DECIMAL(19,4),
+        nav_market_close DECIMAL(19,4),
+        sec_30_day_yield DECIMAL(19,4),
+        distribution_rate DECIMAL(19,4),
+        distribution_rate_12_month DECIMAL(19,4),
+        sec_30_day_unsubsidised_yield DECIMAL(19,4),
+        closing_price DECIMAL(19,4),
+        bid_ask_midpoint DECIMAL(19,4),
+        bid_ask_premium_discount DECIMAL(19,4),
+        bid_ask_premium_discount_pct DECIMAL(19,4),
+        bid_ask_spread_median DECIMAL(19,4),
+        libor_3_moth DECIMAL(19,4),
+        weighted_avg_price DECIMAL(19,4),
+    )
     CREATE TABLE antipodes.dbo.prices (
         FSYM_ID NVARCHAR(64) NOT NULL,
         P_DATE DATE NOT NULL,
