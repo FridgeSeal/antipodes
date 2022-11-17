7ccca282a7d7daa03a227d06e1ccca4686189db3
Tom Watson <tom_w@fastmail.com>
2022-11-17 22:46:29 +1100

    Set language in locale to head off a number of package-install errors

--- dev/run_services.sh
+++ dev/run_services.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -eux
 
+export LANG=en_AU.UTF-8
+
 rstudio-server start
 # Allow root call necessary on jupyter, as root access was a project requirement
 # and jupyter cuts you off from the environment by default
